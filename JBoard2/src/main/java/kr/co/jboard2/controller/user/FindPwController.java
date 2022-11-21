30fb6b6f0ae5e1084ef26de40f1aa2cf1b76d5d1
chhak0503 <chhak0503@gmail.com>
2022-11-21 11:25:16 +0900

    2022/11/21 수업예제

--- JBoard2/src/main/java/kr/co/jboard2/controller/user/FindPwController.java
+++ JBoard2/src/main/java/kr/co/jboard2/controller/user/FindPwController.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/user/findPw.do")
 public class FindPwController extends HttpServlet  {
 	private static final long serialVersionUID = 1L;
 	
@@ -17,7 +19,7 @@ public class FindPwController extends HttpServlet  {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/user/login.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/user/findPw.jsp");
 		dispatcher.forward(req, resp);
 	}
 	
--- JBoard2/src/main/java/kr/co/jboard2/dao/UserDAO.java
+++ JBoard2/src/main/java/kr/co/jboard2/dao/UserDAO.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import kr.co.jboard2.db.DBHelper;
 import kr.co.jboard2.db.Sql;
 import kr.co.jboard2.vo.TermsVO;
+import kr.co.jboard2.vo.UserVO;
 
 public class UserDAO extends DBHelper {
 
@@ -19,7 +20,29 @@ public class UserDAO extends DBHelper {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	
-	public void insertUser() {}
+	public void insertUser(UserVO vo) {
+		try {
+			logger.info("insertUser...");
+			
+			conn = getConnection();
+			psmt = conn.prepareStatement(Sql.INSERT_USER);
+			psmt.setString(1, vo.getUid());
+			psmt.setString(2, vo.getPass());
+			psmt.setString(3, vo.getName());
+			psmt.setString(4, vo.getNick());
+			psmt.setString(5, vo.getEmail());
+			psmt.setString(6, vo.getHp());
+			psmt.setString(7, vo.getZip());
+			psmt.setString(8, vo.getAddr1());
+			psmt.setString(9, vo.getAddr2());
+			psmt.setString(10, vo.getRegip());
+			psmt.executeUpdate();
+			close();
+			
+		}catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
 	
 	public TermsVO selectTerms() {
 		
@@ -47,6 +70,31 @@ public class UserDAO extends DBHelper {
 		return vo;
 	}
 	
+	public int selectCountUid(String uid) {
+		
+		int result = 0;
+		
+		try {
+			logger.info("selectCheckUid");
+			
+			conn = getConnection();
+			psmt = conn.prepareStatement(Sql.SELECT_COUNT_UID);
+			psmt.setString(1, uid);
+			rs = psmt.executeQuery();
+			
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+			
+			close();
+		}catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		
+		logger.debug("result : " + result);
+		
+		return result;
+	}
 	public void selectUser() {}
 	public void selectUsers() {}
 	public void updateUser() {}
