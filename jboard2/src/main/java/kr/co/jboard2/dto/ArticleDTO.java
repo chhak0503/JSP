03db5229059df28ed4d8588bd6337ecac644a11c
chhak0503 <chhak0503@gmail.com>
2024-02-27 17:38:57 +0900

    add

--- jboard2/src/main/java/kr/co/jboard2/dto/ArticleDTO.java
+++ jboard2/src/main/java/kr/co/jboard2/dto/ArticleDTO.java
@@ -18,6 +18,14 @@ public class ArticleDTO {
 	
 	// 추가필드
 	private List<FileDTO> fileDTOs;
+	private String nick;
+	
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
 	
 	public List<FileDTO> getFileDTOs() {
 		return fileDTOs;
